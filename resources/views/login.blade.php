83f3c2fb9766ad89315a1c62169abe62e573a127
Daniils Šarpans <A230246DS@rvt.lv>
2025-10-13 01:08:12 +0300

    Replaced vite.js with plain css

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/css/login.css')
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
     <title>Log In</title>
 </head>
 <body>
