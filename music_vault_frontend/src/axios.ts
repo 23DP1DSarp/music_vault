0371cc948e609d4e88e5442b80a820ef82beb251
Daniils Šarpans <A230246DS@rvt.lv>
2026-04-13 21:28:11 +0300

    Changed the base url

--- music_vault_frontend/src/axios.ts
+++ music_vault_frontend/src/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: 'music-vault-main-sjukhk.laravel.cloud/api',
     headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
